d8736d434adfb3349ac1368ab401e9ae0a59b208
Ziedelth <watteaupierre@gmail.com>
2022-09-16 09:24:15 +0200

    Fix database bug

--- src/main/kotlin/fr/jais/scraper/utils/Database.kt
+++ src/main/kotlin/fr/jais/scraper/utils/Database.kt
@@ -5,8 +5,16 @@ import fr.jais.scraper.entities.Episode
 import java.io.File
 
 class Database {
-    private val file = File("database.json")
     private val gson = GsonBuilder().setPrettyPrinting().create()
+    private val file = File("database.json")
+        get() {
+            if (!field.exists()) {
+                field.createNewFile()
+                field.writeText("[]")
+            }
+
+            return field
+        }
 
     fun load(): MutableList<Episode> = gson.fromJson(file.readText(), Array<Episode>::class.java)?.toMutableList() ?: mutableListOf()
     fun save(episodes: List<Episode>) = file.writeText(gson.toJson(episodes))
