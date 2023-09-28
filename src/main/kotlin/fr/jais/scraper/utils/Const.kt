a1cd615e7cac8ca11e629dee646bb97aa3e29d40
Ziedelth <watteaupierre@gmail.com>
2023-09-28 16:50:57 +0200

    Add Authorization header to API requests

--- src/main/kotlin/fr/jais/scraper/utils/Const.kt
+++ src/main/kotlin/fr/jais/scraper/utils/Const.kt
@@ -18,4 +18,5 @@ object Const {
     val multipleSpaceRegex = "\\s+".toRegex()
     const val calendarBaseUrl = "https://anime.icotaku.com"
     val apiUrl = System.getenv("API_URL") ?: "http://localhost:8080/"
+    val secureKey: String? = System.getenv("SECURE_KEY")
 }
